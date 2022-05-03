165057383fe74e6264b7fc69271793d2f647c883
Akin Fagbohun <a_fagbohun@yahoo.co.uk>
2022-05-03 23:20:02 +0100

    Cleaned up App JS and implemented Stack Navigator.

--- App.js
+++ App.js
@@ -1,32 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import MainContainer from './navigation/MainContainer';
-import Home from './navigation/screens/Home';
+// Login Screen
+import { LoginScreen } from './navigation/screens/LoginScreen';
+
+// React Navigation
+import { NavigationContainer } from '@react-navigation/native';
+
+// Bottom Navigation Component
+import BottomNavigation from './navigation/BottomNavigation';
 
 export default function App() {
+  /* eventually need to track login state here
+  or in global context. In a use effect, when 
+  the log in state changes, the app should trigger
+  a useEffect reload and the user should either see 
+  the login if their action was to log out, or the rest of
+  the app if their action was to sign up or log in. */
+
   return (
     <SafeAreaProvider>
+      {/* expo code starts */}
+      <StatusBar style="auto" />
+      {/* expo code ends */}
       {/* 
       Here, I think we need to write some logic
       to show the login page(s) when user not
       logged in, or to show the rest of the App,
       i.e. the entire view that is immediately below.
-      */}
-      <View style={styles.container}>
-        <MainContainer />
-        <StatusBar style="auto" />
-      </View>
+    */}
+
+      {/* <LoginScreen /> */}
+
+      <NavigationContainer>
+        <BottomNavigation />
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // justifyContent: 'space-between',
-  },
-});
---
+++ navigation/BottomNavigation.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// Screens
+import { HomeStackScreen } from './screens/HomeScreen';
+import { SecondScreen } from './screens/SecondScreen';
+import { ChatScreen } from './screens/ChatScreen';
+import { FourthScreen } from './screens/FourthScreen';
+
+// Screen Names
+const HomeTab = 'Home';
+const SecondTab = 'Second';
+const ChatTab = 'Chat';
+const FourthTab = 'Fourth';
+
+// Bottom Nav
+const Tab = createBottomTabNavigator();
+
+const BottomNavigation = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName={HomeTab}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          let rn = route.name;
+
+          if (rn === HomeTab) {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (rn === SecondTab) {
+            iconName = focused ? 'list' : 'list-outline';
+          } else if (rn === ChatTab) {
+            iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
+          } else if (rn === FourthTab) {
+            iconName = focused ? 'bug' : 'bug-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}>
+      <Tab.Screen
+        name={HomeTab}
+        component={HomeStackScreen}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen name={SecondTab} component={SecondScreen} />
+      <Tab.Screen name={ChatTab} component={ChatScreen} />
+      <Tab.Screen name={FourthTab} component={FourthScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomNavigation;
