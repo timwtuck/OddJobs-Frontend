5b5cccf2e5e1e1c5ab5ade29d1f7c98da7f189bd
vbrooke78 <vscotter@hotmail.com>
2022-05-04 22:21:31 +0100

    this commit has enabled markers to be displayed dynamically, custom styling added - will need updating once userContext is available

--- components/Map.js
+++ components/Map.js
@@ -1,17 +1,57 @@
 import MapView from 'react-native-maps';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { Marker } from 'react-native-maps';
+import { useEffect, useState } from 'react';
+import { getAllJobs } from '../api';
 
 export const Map = () => {
+  const [jobs, setJobs] = useState([]);
+
+  const userLocation = { latitude: 53.8008, longitude: -1.5491 };
+
+  useEffect(() => {
+    getAllJobs().then(jobsFromApi => {
+      setJobs(jobsFromApi);
+    });
+  }, []);
+
+  const locations = jobs.map(({ title, location, _id }) => ({
+    title,
+    location,
+    _id,
+  }));
+
   return (
     <MapView
       style={styles.map}
       region={{
-        latitude: 53.8008,
-        longitude: -1.5491,
+        latitude: userLocation.latitude,
+        longitude: userLocation.longitude,
         latitudeDelta: 0.015,
         longitudeDelta: 0.0121,
-      }}
-    />
+      }}>
+      <Marker
+        key={'user_id'}
+        coordinate={{
+          latitude: userLocation.latitude,
+          longitude: userLocation.longitude,
+        }}
+        title={'My location'}
+      />
+      {locations.map(location => {
+        return (
+          <Marker
+            key={location._id}
+            coordinate={{
+              latitude: location.location.latitude,
+              longitude: location.location.longitude,
+            }}
+            title={location.title}>
+            <Text>🎩</Text>
+          </Marker>
+        );
+      })}
+    </MapView>
   );
 };
 
