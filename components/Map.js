1a7ffee2af20405f227b9fad807fdb552bcc2d19
vbrooke78 <vscotter@hotmail.com>
2022-05-08 21:28:49 +0100

    this commit has updated the map component to pin the users location to the map using the geocoding api

--- components/Map.js
+++ components/Map.js
@@ -2,12 +2,27 @@ import MapView from 'react-native-maps';
 import { StyleSheet, View, Text } from 'react-native';
 import { Marker } from 'react-native-maps';
 import { useEffect, useState } from 'react';
-import { getAllJobs } from '../api';
+import { convertFromPostcode, getAllJobs, getSingleUser } from '../api';
+import { useContext } from 'react';
+import { AuthContext } from '../App';
+import axios from 'axios';
 
 export const Map = () => {
   const [jobs, setJobs] = useState([]);
+  const [postcode, setPostcode] = useState('');
+  const [userLocation, setUserLocation] = useState('');
 
-  const userLocation = { latitude: 53.8008, longitude: -1.5491 };
+  // global user context
+  const user = useContext(AuthContext);
+  // console.log(user);
+  // global user context
+
+  //get user postcode and convert to valid address with '%20' delimited spaces
+  useEffect(() => {
+    getSingleUser(user._id).then(userFromApi => {
+      setPostcode(userFromApi.postCode);
+    });
+  }, [user._id]);
 
   useEffect(() => {
     getAllJobs().then(jobsFromApi => {
@@ -15,30 +30,46 @@ export const Map = () => {
     });
   }, []);
 
-  const locations = jobs.map(({ title, location, _id }) => ({
+  //request to geocodeAPI to convert postcode to lat/long
+  useEffect(() => {
+    axios
+      .get(`https://maps.googleapis.com/maps/api/geocode/json`, {
+        params: {
+          address: postcode,
+          key: `AIzaSyB4DQQ-RhN7z-Btr9Rtz7ghkPno-Cblq1w`,
+        },
+      })
+      .then(response => {
+        setUserLocation(response.data.results[0].geometry.location, 'axios');
+      });
+  }, []);
+
+  const locations = jobs.map(({ title, postcode, _id }) => ({
     title,
-    location,
+    postcode,
     _id,
   }));
+
+  console.log(locations);
 
   return (
     <MapView
       style={styles.map}
       region={{
-        latitude: userLocation.latitude,
-        longitude: userLocation.longitude,
+        latitude: userLocation.lat,
+        longitude: userLocation.lng,
         latitudeDelta: 0.015,
         longitudeDelta: 0.0121,
       }}>
       <Marker
         key={'user_id'}
         coordinate={{
-          latitude: userLocation.latitude,
-          longitude: userLocation.longitude,
+          latitude: userLocation.lat,
+          longitude: userLocation.lng,
         }}
         title={'My location'}
       />
-      {locations.map(location => {
+      {/* {locations.map(location => {
         return (
           <Marker
             key={location._id}
@@ -50,7 +81,7 @@ export const Map = () => {
             <Text>🎩</Text>
           </Marker>
         );
-      })}
+      })} */}
     </MapView>
   );
 };
