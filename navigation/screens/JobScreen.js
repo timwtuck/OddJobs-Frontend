0b5133a0e9e6579beae83a2e0e72e741f500d8b1
vbrooke78 <vscotter@hotmail.com>
2022-05-10 11:58:01 +0100

    images styled

--- navigation/screens/JobScreen.js
+++ navigation/screens/JobScreen.js
@@ -1,8 +1,23 @@
 import * as React from 'react';
 import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../App';
-import { Button, Image, Text, View, StyleSheet } from 'react-native';
+import {
+  Button,
+  Dimensions,
+  Image,
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
 import { getSingleJob } from '../../api';
+import {
+  Cleaning,
+  Delivery,
+  DIY,
+  Garden,
+  Pets,
+  Shopping,
+} from '../../components/categoryImages';
 
 // // Custom Fonts
 // import AppLoading from 'expo-app-loading';
@@ -36,55 +51,27 @@ export const JobScreen = ({ route, navigation }) => {
 
   return (
     <View style={styles.jobContainer}>
-      <View style={styles.imageContainer}>
-        {currentJob.category === 'Cleaning' && (
-          <Image
-            style={styles.categoryImage}
-            source={require('../../assets/Cleaning.png')}
-          />
-        )}
-        {currentJob.category === 'Delivery' && (
-          <Image
-            style={styles.categoryImage}
-            source={require('../../assets/Delivery.png')}
-          />
-        )}
-        {currentJob.category === 'DIY' && (
-          <Image
-            style={styles.categoryImage}
-            source={require('../../assets/DIY.png')}
-          />
-        )}
-        {currentJob.category === 'Garden' && (
-          <Image
-            style={styles.categoryImage}
-            source={require('../../assets/Garden.png')}
-          />
-        )}
-        {/* {currentJob.category === 'Pets' && (
-          <Image
-            style={styles.categoryImage}
-            source={require('../../assets/Pets.png')}
-          />
-        )} */}
-        {currentJob.category === 'Shopping' && (
-          <Image
-            style={styles.categoryImage}
-            source={require('../../assets/Shopping.png')}
-          />
-        )}
-        {currentJob.category === 'Other' && (
+      {currentJob.category === 'Cleaning' && <Cleaning />}
+      {currentJob.category === 'Delivery' && <Delivery />}
+      {currentJob.category === 'DIY' && <DIY />}
+      {currentJob.category === 'Garden' && <Garden />}
+      {currentJob.category === 'Pets' && <Pets />}
+      {currentJob.category === 'Shopping' && <Shopping />}
+      {currentJob.category === 'Other' && (
+        <View style={styles.imageContainer}>
           <Image
             style={styles.categoryImage}
             source={require('../../assets/logo.png')}
           />
-        )}
+        </View>
+      )}
+      {/* </View> */}
+      <View style={styles.imageContainer}>
+        <Text style={styles.jobHeading}>{currentJob.title}</Text>
+        <Text style={styles.jobCategory}>{currentJob.category}</Text>
+        <Text style={styles.jobDescription}>{currentJob.description}</Text>
+        <Text style={styles.jobPrice}>{currentJob.price}</Text>
       </View>
-
-      <Text style={styles.jobHeading}>{currentJob.title}</Text>
-      <Text style={styles.jobCategory}>{currentJob.category}</Text>
-      <Text style={styles.jobDescription}>{currentJob.description}</Text>
-      <Text style={styles.jobPrice}>{currentJob.price}</Text>
       {currentJob.user_id === user._id && <Button title="🗑" />}
     </View>
   );
@@ -98,15 +85,28 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     alignItems: 'center',
   },
-  imageContainer: {
-    justifyContent: 'flex-start',
-    height: '20%',
-  },
   header: {
     fontSize: 20,
   },
+  imageContainer: {
+    backgroundColor: '#FFEDDF',
+    width: Dimensions.get('window').width * 0.9,
+    height: 220,
+    marginVertical: 10,
+    padding: 10,
+    borderRadius: 15,
+  },
   categoryImage: {
     alignItems: 'center',
+    justifyContent: 'center',
+    resizeMode: 'contain',
+    width: '95%',
+
+    backgroundColor: 'white',
+    marginTop: 5,
+
+    marginHorizontal: 5,
+    borderRadius: 10,
   },
   jobHeading: {
     fontSize: 30,
